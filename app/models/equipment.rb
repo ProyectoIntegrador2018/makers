735103fd1f758e0fec732acd0456de99296b6b7b
Chuy Galván <jgalvan@users.noreply.github.com>
2019-03-02 00:16:08 -0600

    Update app/models/equipment.rb
    Co-Authored-By: anakarenbm <anakarenbmak@yahoo.com>

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,7 +1,7 @@
 class Equipment < ApplicationRecord
   has_many :equipment_materials, dependent: :destroy
   has_many :materials, through: :equipment_materials
-  has_many :equipment_capabilities
+  has_many :equipment_capabilities, dependent: :destroy
   has_many :capabilities, through: :equipment_capabilities
   belongs_to :lab_space
 
