adb81351529923f20a02ed056a1efed485c57cf1
jgalvan <galvan.vil@gmail.com>
2019-03-24 19:25:39 -0600

    Add role and names to users; generate devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:given_name, :last_name, :email, :password)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:given_name, :last_name, :email, :password, :current_password)
+    end
+  end
+
 end
