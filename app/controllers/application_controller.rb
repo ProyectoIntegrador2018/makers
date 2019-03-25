af248ced6ae0fe81ed59a02ecc6d005981cc7a2a
jgalvan <galvan.vil@gmail.com>
2019-03-24 19:35:31 -0600

    Fix reek issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:given_name, :last_name, :email, :password)
+    devise_parameter_sanitizer.permit(:sign_up) do |user|
+      user.permit(:given_name, :last_name, :email, :password)
     end
 
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:given_name, :last_name, :email, :password, :current_password)
+    devise_parameter_sanitizer.permit(:account_update) do |user|
+      user.permit(:given_name, :last_name, :email, :password, :current_password)
     end
   end
-
 end
