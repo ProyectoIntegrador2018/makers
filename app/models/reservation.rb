d65c90d9f2df6468842f29b829190957793bfb8e
Ana Karen <anakarenbmak@yahoo.com>
2019-03-25 10:12:05 -0600

    Add enums for reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,7 @@
 class Reservation < ApplicationRecord
+  enum status: [:arrived, :pending, :cancelled]
+  enum type: [:normal, :block, :cancelled]
+
   belongs_to :equipment
   belongs_to :user
 end
