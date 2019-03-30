341f4266074a26e17f580993a447233b9047557f
jgalvan <galvan.vil@gmail.com>
2019-03-29 20:52:18 -0600

    Fix bug 41: missing allowed user params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user|
-      user.permit(:given_name, :last_name, :email, :password)
+      user.permit(:given_name, :last_name, :email, :institutional_id, :password, :password_confirmation)
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |user|
-      user.permit(:given_name, :last_name, :email, :password, :current_password)
+      user.permit(:given_name, :last_name, :email, :institutional_id, :password, :password_confirmation, :current_password)
     end
   end
 end
