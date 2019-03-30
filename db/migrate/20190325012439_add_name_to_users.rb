30d4055d5c951378e4eb37fd737ce7bc4ff81f10
Hector Rincon <hecerinc@hotmail.com>
2019-03-29 20:59:57 -0600

    Add matricula & names to registration form. Fix non-ascii chars

--- db/migrate/20190325012439_add_name_to_users.rb
+++ db/migrate/20190325012439_add_name_to_users.rb
@@ -1,6 +1,6 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :given_name, :string
-    add_column :users, :last_name, :string
+    add_column :users, :given_name, :string, null: false
+    add_column :users, :last_name, :string, null: false
   end
 end
