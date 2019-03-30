61cc1f7cc07457459e13d920d19f56e49fd0dc6e
Hector Rincon <hecerinc@hotmail.com>
2019-03-30 12:41:43 -0600

    Add seeder for base data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+2.times do
+  lab = Lab.create(
+    name: Faker::App.name,
+    description: Faker::Lorem.sentence(30, true, 10),
+    location: "CIAP 628-F",
+    image: Faker::LoremPixel.image("300x260")
+  )
+
+  ls = lab.lab_spaces.create(
+    name: Faker::App.name,
+    description: Faker::Lorem.sentence(30, true, 10),
+    hours: "Lunes a Viernes: 8 AM - 5:30 PM",
+    location: "Innovaction Gym",
+    contact_email: "azael.capetillo@tec.mx",
+    contact_phone: Faker::PhoneNumber.cell_phone,
+    image: Faker::LoremPixel.image("300x260")
+  )
+
+  6.times do
+    eq = ls.equipment.create(
+      name: Faker::App.name,
+      description: Faker::Lorem.sentence(100, true, 10),
+      image: Faker::LoremPixel.image("300x260"),
+      technical_description: Faker::Lorem.sentence(300, true, 15)
+    )
+    5.times do |i|
+      eq.available_hours.create(
+        day_of_week: (i+1),
+        start_time: "08:30",
+        end_time: "17:30"
+      )
+    end
+  end
+end
