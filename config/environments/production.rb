6dd3e4fcd45821415167be9195c017d69b3ba9ef
jgalvan <galvan.vil@gmail.com>
2019-03-30 13:57:17 -0600

    Add configuration for sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PSW'],
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
