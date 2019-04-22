26ce44639f846a6c74cfd876388ce2282f35519e
Hector Rincon <hecerinc@hotmail.com>
2019-04-22 15:47:46 -0500

    Try config of host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -100,4 +100,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { host: 'makers-program-staging.herokuapp.com' }
 end
