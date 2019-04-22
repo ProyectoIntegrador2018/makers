4346a7b9669a7308c131a4223bad20af938946d3
Hector Rincon <hecerinc@hotmail.com>
2019-04-21 20:18:51 -0500

    Add home search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
   config.cache_store = :null_store
   def landing
     @body_class = 'Home'
+    @capabilities = Capability.all
+    @materials = Material.all
     render layout: false
   end
 end
