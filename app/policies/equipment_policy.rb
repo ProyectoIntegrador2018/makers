3f6f75030f7b341744315e46ddf4e30d0a6a1699
jgalvan <galvan.vil@gmail.com>
2019-04-25 03:38:55 -0500

    Filter dashboard navbar by permissions

--- app/policies/equipment_policy.rb
+++ app/policies/equipment_policy.rb
@@ -1,6 +1,12 @@
 class EquipmentPolicy < ManagedModelsPolicy
   def new?
     return false unless user
+
+    user.superadmin? || user.admin? || user.lab_admin?
+  end
+
+  def create?
+    return false unless user
     return user.manages?(record.lab_space) if user.admin? || user.lab_admin?
 
     user.superadmin?
