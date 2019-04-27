b85b9d55e1af1de270aa7c719b459fa633a4a6c7
jgalvan <galvan.vil@gmail.com>
2019-04-26 20:46:17 -0500

    Change EquipmentMaterial/EquipmentCapability clear to destroy all for callbacks and add uniqueness validations.

--- app/models/equipment_capability.rb
+++ app/models/equipment_capability.rb
@@ -2,6 +2,7 @@ class EquipmentCapability < ApplicationRecord
   belongs_to :capability
   belongs_to :equipment
   validates :capability, :equipment, presence: true
+  validates_uniqueness_of :capability_id, scope: :equipment_id
   after_destroy :destroy_orphaned
 
   def destroy_orphaned
