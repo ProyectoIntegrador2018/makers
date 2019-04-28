b47ad4df9667fa91cc7457b2508381b2bd0af1fa
Hector Rincon <hecerinc@hotmail.com>
2019-04-27 22:01:19 -0500

    More intuitive form for available hours. Create equipment works now

--- app/fields/belongs_to_with_user_field.rb
+++ app/fields/belongs_to_with_user_field.rb
@@ -6,6 +6,8 @@ class BelongsToWithUserField < Administrate::Field::BelongsTo
   protected
 
   def candidate_resources
+    # If this method fails, check if you're calling `render_field attribute`
+    # if you are, make sure to call `check_attribute(attribute)` right before `render_field`
     @current_user.try(options[:scope_name])
   end
 end
