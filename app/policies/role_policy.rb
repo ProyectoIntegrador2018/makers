90cc5e93023c4ff95c219a7fd6176e499e5a38a9
Aldo Cervantes Barbosa <aldo.cervantes@ext.cemex.com>
2019-10-15 16:07:38 -0500

    Remove the ability for a Lab Admin to upgrade another user to Lab_admin

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -10,8 +10,7 @@ class RolePolicy
     def resolve
       roles = User.roles.keys.map { |role| [role.titleize, role] }
       return roles if user.superadmin?
-
-      roles - [%w[Superadmin superadmin]] if user.lab_admin?
+      roles - [["Superadmin", "superadmin"], ["Lab Admin", "lab_admin"]] if user.lab_admin?
     end
 
     def resolve_admin
