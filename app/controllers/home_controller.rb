202a71175288977bb9b3f8cdfde0954939d90721
Diego Astiazaran <diegoaat97@gmail.com>
2019-10-16 21:52:14 -0500

    Fix code climate issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,50 +12,57 @@ class HomeController < ApplicationController
     @reservations = current_user.reservations.confirmed.future
   end
 
+  def get_queried_items
+    type = params[:type]
+    type_is_capability = type == 'capabilities'
+    type_table = type_is_capability ? Capability : Material
+    selected_item = params[:selectedItem]
+    # Check if an item of the other category was chosen
+    if selected_item.present?
+      # Define some variables
+      other_type = type_is_capability ? 'materials' : 'capabilities'
+      type_id = type.singularize + '_id'
+      other_type_id = other_type.singularize + '_id'
+      equipment_type_table = type_is_capability ? EquipmentCapability : EquipmentMaterial
+      other_equipment_type_table = type_is_capability ? EquipmentMaterial : EquipmentCapability
+
+      # Get equipment ids that use the selected item
+      equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
+      # Get the desired items of the equipments selected
+      item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map{ |item| item[type_id] }
+      # Get the name of the items selected
+      results = type_table.select(:id, :name).where(id: item_ids)
+    else
+      # Get all the results
+      results = type_table.select(:id, :name)
+    end
+
+    query = params[:query]
+    if query.present?
+      # Filter results by query written
+      results = results.where('name ILIKE ?', "%#{query}%").as_json
+    end
+    return results
+  end
+
   def related
     type = params[:type]
 
     if type != 'capabilities' && type != 'materials'
-      @capabilities = Capability.select(:id, :name).as_json
-      @materials = Material.select(:id, :name).as_json
+      capabilities = Capability.select(:id, :name).as_json
+      materials = Material.select(:id, :name).as_json
     else
-      type_table = type == 'capabilities' ? Capability : Material
-      selected_item = params[:selectedItem]
-      # Check if an item of the other category was chosen
-      if selected_item.present?
-        # Define some variables
-        other_type = type == 'capabilities' ? 'materials' : 'capabilities'
-        type_id = type.singularize() + '_id'
-        other_type_id = other_type.singularize() + '_id'
-        equipment_type_table = type == 'capabilities' ? EquipmentCapability : EquipmentMaterial
-        other_equipment_type_table = type == 'capabilities' ? EquipmentMaterial : EquipmentCapability
-
-        # Get equipment ids that use the selected item
-        equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
-        # Get the desired items of the equipments selected
-        item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map{|i| i[type_id]}
-        # Get the name of the items selected
-        @results = type_table.select(:id, :name).where(id: item_ids)
-      else
-        # Get all the results
-        @results = type_table.select(:id, :name)
-      end
-
-      query = params[:query]
-      if query.present?
-        # Filter results by query written
-        @results = @results.where('name ILIKE ?', "%#{query}%").as_json
-      end
+      results = get_queried_items
     end
 
     respond_to do |format|
-      format.json { render json: {
-        results: @results,
-        capabilities: @capabilities,
-        materials: @materials
-        }
+      format.json {
+        render json: {
+        results: results,
+        capabilities: capabilities,
+        materials: materials
       }
+    }
     end
   end
-
 end
