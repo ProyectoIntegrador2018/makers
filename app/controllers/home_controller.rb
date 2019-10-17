e14ec9cc60173d827def915672d81fa1747e8749
Diego Astiazaran <diegoaat97@gmail.com>
2019-10-16 23:34:54 -0500

    Fix code climate issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,6 +12,21 @@ class HomeController < ApplicationController
     @reservations = current_user.reservations.confirmed.future
   end
 
+  def apply_query(selected_item, type_table, type_id, other_type_id, equipment_type_table, other_equipment_type_table)
+    # Get all the results
+    results = type_table.select(:id, :name)
+    # Check if an item of the other category was chosen
+    if selected_item.present?
+      # Get equipment ids that use the selected item
+      equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
+      # Get the desired items of the equipments selected
+      item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
+      # Get the name of the items selected
+      results = results.where(id: item_ids)
+    end
+    results
+  end
+
   def queried_items
     # Define some variables
     type = params[:type]
@@ -30,18 +45,7 @@ class HomeController < ApplicationController
     other_type_id = other_type.singularize + '_id'
     selected_item = params[:selectedItem]
 
-    # Get all the results
-    results = type_table.select(:id, :name)
-    # Check if an item of the other category was chosen
-    if selected_item.present?
-      # Get equipment ids that use the selected item
-      equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
-      # Get the desired items of the equipments selected
-      item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
-      # Get the name of the items selected
-      results = results.where(id: item_ids)
-    end
-    results
+    apply_query(selected_item, type_table, type_id, other_type_id, equipment_type_table, other_equipment_type_table)
   end
 
   def related
