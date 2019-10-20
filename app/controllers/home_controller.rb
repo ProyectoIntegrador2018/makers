d4dd1886da3a4de061c27ded02e54a3734b14131
Diego Astiazaran <diegoaat97@gmail.com>
2019-10-20 18:12:18 -0500

    Fix code climate issues

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,18 +12,18 @@ class HomeController < ApplicationController
     @reservations = current_user.reservations.confirmed.future
   end
 
-  def apply_query(options = {})
+  def apply_query(type_table, equipment_type_table, other_equipment_type_table, other_type_id)
     # Define some variables
     type_id = params[:type].singularize + '_id'
     selected_item = params[:selectedItem]
     # Get all the results
-    results = options[:type_table].select(:id, :name)
+    results = type_table.select(:id, :name)
     # Check if an item of the other category was chosen
     if selected_item.present?
       # Get equipment ids that use the selected item
-      equipments = options[:other_equipment_type_table].select(:equipment_id).where("#{options[:other_type_id]} = ?", selected_item).map(&:equipment_id)
+      equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
       # Get the desired items of the equipments selected
-      item_ids = options[:equipment_type_table].select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
+      item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
       # Get the name of the items selected
       results = results.where(id: item_ids)
     end
@@ -32,9 +32,9 @@ class HomeController < ApplicationController
 
   def queried_items
     if params[:type] == 'capabilities'
-      apply_query(type_table: Capability, equipment_type_table: EquipmentCapability, other_equipment_type_table: EquipmentMaterial, other_type_id: 'material_id')
+      apply_query(Capability, EquipmentCapability, EquipmentMaterial, 'material_id')
     else
-      apply_query(type_table: Material, equipment_type_table: EquipmentMaterial, other_equipment_type_table: EquipmentCapability, other_type_id: 'capability_id')
+      apply_query(Material, EquipmentMaterial, EquipmentCapability, 'capability_id')
     end
   end
 
