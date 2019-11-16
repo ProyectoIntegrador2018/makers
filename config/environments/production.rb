afcc535ebf1bde604273db09c45458c287000058
Hector Mg <hectormgwake@gmail.com>
2019-11-15 20:12:57 -0600

    Change sendgrid env var name in production environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PSW'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
