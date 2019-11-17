1ded2b60c70dc920f89a2f06f2578d275805b3e9
Héctor Morales <hectormgdev@gmail.com>
2019-11-17 16:20:36 -0600

    Update deployment configuration (#164)
    * Fix bug in Lab Space dashboard view that caused the app to crash because the user attribute wasn't listed in the dashboard attributes configuration
    
    * Change sendgrid env var name in production environment configuration
    
    * Update Uglifier configuration for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -94,7 +94,7 @@ Rails.application.configure do
 
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PSW'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
