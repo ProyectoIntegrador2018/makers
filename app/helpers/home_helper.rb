7be791299f9f7e25f3882e2342fc304fed7c97f5
Diego Astiazaran <diegoaat97@gmail.com>
2019-11-19 05:19:28 -0600

    Reduce complexity of method

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,14 +6,12 @@ module HomeHelper
     # Get all the results
     results = options[:type_table].select(:id, :name)
     # Check if an item of the other category was chosen
-    if selected_item.present?
-      # Get equipment ids that use the selected item
-      equipments = options[:other_equipment_type_table].select(:equipment_id).where("#{options[:other_type_id]} = ?", selected_item).map(&:equipment_id)
-      # Get the desired items of the equipments selected
-      item_ids = options[:equipment_type_table].select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
-      # Get the name of the items selected
-      results = results.where(id: item_ids)
-    end
-    results
+    return results unless selected_item.present?
+    # Get equipment ids that use the selected item
+    equipments = options[:other_equipment_type_table].select(:equipment_id).where("#{options[:other_type_id]} = ?", selected_item).map(&:equipment_id)
+    # Get the desired items of the equipments selected
+    item_ids = options[:equipment_type_table].select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
+    # Get the name of the items selected
+    results.where(id: item_ids)
   end
 end
