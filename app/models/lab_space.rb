ad4f3927f74dd37e2eadf17ea3dba2a6097ea469
Diego Astiazarán <diegoaat97@gmail.com>
2019-11-19 16:39:24 -0600

    Make user optional in user and lab space (#172)

--- app/models/lab_space.rb
+++ app/models/lab_space.rb
@@ -5,8 +5,7 @@ class LabSpace < ApplicationRecord
 
   belongs_to :lab
   has_many :equipment
-  belongs_to :user
-  
+  belongs_to :user, optional: true
 
   has_many :lab_administrations, as: :space
   has_many :admins, through: :lab_administrations
