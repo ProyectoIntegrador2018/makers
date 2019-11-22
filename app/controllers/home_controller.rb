d39f0f775f825c6eb73f1019c98b6f80e1745026
Diego Astiazaran <diegoaat97@gmail.com>
2019-11-21 22:10:43 -0600

    Filter for any selected tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 # Home controller for static views
 class HomeController < ApplicationController
+  include HomeHelper
   config.cache_store = :null_store
   before_action :authenticate_user!, except: [:landing, :related, :equipment_relation]
 
@@ -14,40 +15,16 @@ class HomeController < ApplicationController
     @reservations = @all_reservations.rejected.future + @all_reservations.pending.future + @all_reservations.confirmed.future
   end
 
-  def apply_query(type_table, equipment_type_table, other_equipment_type_table, other_type_id)
-    # Define some variables
-    type_id = params[:type].singularize + '_id'
-    selected_item = params[:selectedItem]
-    # Get all the results
-    results = type_table.select(:id, :name)
-    # Check if an item of the other category was chosen
-    if selected_item.present?
-      # Get equipment ids that use the selected item
-      equipments = other_equipment_type_table.select(:equipment_id).where("#{other_type_id} = ?", selected_item).map(&:equipment_id)
-      # Get the desired items of the equipments selected
-      item_ids = equipment_type_table.select(type_id).where(equipment_id: equipments).map { |item| item[type_id] }
-      # Get the name of the items selected
-      results = results.where(id: item_ids)
-    end
-    results
-  end
-
-  def queried_items
-    if params[:type] == 'capabilities'
-      apply_query(Capability, EquipmentCapability, EquipmentMaterial, 'material_id')
-    else
-      apply_query(Material, EquipmentMaterial, EquipmentCapability, 'capability_id')
-    end
-  end
-
   def related
     type = params[:type]
-
-    if type != 'capabilities' && type != 'materials'
+    if type == 'all'
       capabilities = Capability.select(:id, :name).as_json
       materials = Material.select(:id, :name).as_json
-    else
-      results = queried_items
+    elsif type == 'both'
+      capabilities = apply_caps_mats_query('capabilities', params[:selectedMat])
+      materials = apply_caps_mats_query('materials', params[:selectedCap])
+    else # capabilities or materials
+      results = apply_caps_mats_query(type, params[:selectedItem])
       query = params[:query]
       if query.present?
         # Filter results by query written
@@ -61,18 +38,6 @@ class HomeController < ApplicationController
           results: results,
           capabilities: capabilities,
           materials: materials
-        }
-      end
-    end
-  end
-
-  def equipment_relation
-    equipments_capabilities = EquipmentCapability.select(:equipment_id).where(capability_id: params[:capability]).map(&:equipment_id)
-    equipments_materials = EquipmentMaterial.select(:equipment_id).where(material_id: params[:material]).map(&:equipment_id)
-    respond_to do |format|
-      format.json do
-        render json: {
-          are_related: !(equipments_capabilities & equipments_materials).empty?
         }
       end
     end
