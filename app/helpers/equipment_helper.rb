9c787cbea4f386ec65018288923d8b4413dbee0d
Hector Mg <hectormgwake@gmail.com>
2019-11-23 22:30:22 -0600

    Fix some codeclimate issues

--- app/helpers/equipment_helper.rb
+++ app/helpers/equipment_helper.rb
@@ -1,8 +1,8 @@
 module EquipmentHelper
-  def filter_reservations_for_user(res)
-    reservations = res.confirmed.or(res.blocked).or(res.pending)
-    reservations.select do |res|
-      if(res.status == "pending")
+  def filter_reservations_for_user(reservations)
+    viable_reservations = reservations.confirmed.or(reservations.blocked).or(reservations.pending)
+    viable_reservations.select do |res|
+      if (res.status == "pending")
         res.user == current_user
       else
         true
@@ -13,20 +13,20 @@ module EquipmentHelper
   def set_reservation_title(res)
     if (user_signed_in? && current_user.id == res.user_id)
       title = "#{current_user.given_name} #{current_user.last_name}"
-      if(res.status == "pending")
+      if (res.status == "pending")
         title = "#{title} (pendiente)"
-        end
-      else
-        title = "Reservación"
       end
+    else
+      title = "Reservación"
+    end
     return title
   end
 
   def set_reservation_color(res)
-    if(res.status == "pending")
+    if (res.status == "pending")
       '#bdc3c7'
     else
       'rgba(68, 93, 252, .77)'
-      end
     end
+  end
 end
