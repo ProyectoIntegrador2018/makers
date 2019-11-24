ee1a241a73dd50db61bb39a1865ae2a488a44c4e
Hector Mg <hectormgwake@gmail.com>
2019-11-23 22:59:14 -0600

    Remove equipment helper methods and place the logic in the show view

--- app/helpers/equipment_helper.rb
+++ app/helpers/equipment_helper.rb
@@ -1,21 +1,2 @@
 module EquipmentHelper
-  def filter_reservations_for_user(reservations)
-    viable_reservations = reservations.confirmed.or(reservations.blocked).or(reservations.pending)
-    viable_reservations.select do |reservation|
-      reservation.status == 'pending' ? (reservation.user == current_user) : true
-    end
-  end
-
-  def reservation_title(res)
-    if user_signed_in? && current_user.id == res.user_id
-      title = "#{current_user.given_name} #{current_user.last_name}"
-      "#{title} (pendiente)" if res.status == 'pending'
-    else
-      'Reservación'
-    end
-  end
-
-  def reservation_color(res)
-    res.status == 'pending' ? '#bdc3c7' : 'rgba(68, 93, 252, .77)'
-  end
 end
