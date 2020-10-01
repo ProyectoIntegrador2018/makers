fba456b3becd54f638a52e3f1a4f31713d301c58
Javier Alejandro Lara Cuevas <alexlara_97@hotmail.com>
2020-10-01 18:23:57 -0500

    Changed Mail Service to Use Google
    Added the configuration needed to use google services to send mails trough SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,5 +61,14 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV['GMAIL_USERNAME'],
+    :password => ENV['GMAIL_PASSWORD'],
+    :address => 'smtp.gmail.com',
+    :port => 587,
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
