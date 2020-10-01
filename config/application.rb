b43c3225d2399809c170e02c5356c2af45a50869
Javier Alejandro Lara Cuevas <alexlara_97@hotmail.com>
2020-10-01 18:32:39 -0500

    Added Enviormental Varaibles
    Added the function to read specific Ruby enviorment variables and updated the README to mention this changes

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,13 @@ Bundler.require(*Rails.groups)
 
 module Makersprogram
   class Application < Rails::Application
+    # Loading of Enviorment Variables for use in Development 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.fallbacks = [:en]
